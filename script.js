48785f87b93c6fec3473eadc0fd5fc53b656675b
PaulaA2511 <paulaalejandracaceresmendez201@gmail.com>
2025-07-05 14:59:53 -0500

    Script.js

--- script.js
+++ script.js
@@ -49,3 +49,101 @@ switch(opción){
         alert("Esa opción no existe")
         break
 }
+
+body {
+  font-family: 'Press Start 2P', cursive;
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(135deg, #a17ddc, #a7173e);
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+h1 {
+  font-size: 2rem;
+  margin: 20px 0;
+  color: #facc15;
+  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5);
+}
+
+.slider{
+    position: relative;
+    width: 80%;
+    max-width: 800px;
+    overflow: hidden;
+    border-radius: 20px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5);
+}
+
+.slides {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+}
+
+.slides img {
+  flex: 0 0 100%;
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 20px;
+}
+
+button {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    background: rgba(0, 0, 0, 0.7);
+    color: #facc15;
+    border: none;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    font-size: 2rem;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: transform 0.3s, background 0.3s;
+    z-index: 1000;
+}
+
+button:hover {
+  background: #facc15;
+  color: black;
+  transform: scale(1.2);
+}
+
+.prev {
+    left: 15px;
+}
+
+.next {
+    right: 15px;
+}
+
+.indicators{
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    gap: 15px;
+}
+
+.indicator {
+    width: 12px;
+     height: 12px;
+     background: white;
+     border-radius: 50%;
+     opacity: 0.5;
+     transition: transform 0.3s ease-in-out;
+}
+
+.indicator-active {
+     background: #facc15;
+    transform: scale(1.3);
+     opacity: 1;
+}
